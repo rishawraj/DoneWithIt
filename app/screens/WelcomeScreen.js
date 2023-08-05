3f54b9b6361652ec8b6bc56697aab08f881af60a
rishawraj <rishawraj0703@gmail.com>
2023-08-05 23:13:03 +0530

    basic setup

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Button,
+} from "react-native";
+import colors from "../config/colors";
+
+function WelcomeScreen({ navigation }) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>sell what you don't need</Text>
+        <Button
+          title="image"
+          onPress={() => {
+            navigation.navigate("Image", {
+              name: "Image",
+            });
+          }}
+        />
+      </View>
+
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.secondary,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+});
+
+export default WelcomeScreen;
